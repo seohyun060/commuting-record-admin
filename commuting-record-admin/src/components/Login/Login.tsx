034d2baa63a4d7fb548b84fbc22faedaf5f7b095
Seohyun Park <seohyun060@naver.com>
2022-09-21 15:25:29 +0900

    feat(login): 로그인 퍼블리싱

---
+++ commuting-record-admin/src/components/Login/Login.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useCallback } from 'react';
+import logo from '../../assets/images/logo.png';
+import ic from '../../assets/images/ic.png';
+import './styles/Login.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+function Login() {
+  const [email, setemail] = useState('');
+  const navigate = useNavigate();
+
+  const [pw, setpw] = useState('');
+  const onChangeEmail = useCallback((e: any) => {
+    setemail(e.target.value);
+  }, []);
+
+  const onChangePw = useCallback((e: any) => {
+    setpw(e.target.value);
+  }, []);
+
+  const onLogInClicked = useCallback(() => {
+    if (email !== '' && pw !== '') {
+      navigate('/Home');
+      setemail('');
+      setpw('');
+    }
+  }, [navigate]);
+
+  const onKeyPress = useCallback(
+    (e: any) => {
+      if (e.key === 'Enter') {
+        if (email !== '' && pw !== '') {
+          navigate('/Home');
+          setemail('');
+          setpw('');
+        }
+      }
+    },
+    [navigate],
+  );
+
+  return (
+    <div className='App'>
+      <div className='Container'>
+        <img src={logo}></img>
+        <input
+          className='Email'
+          placeholder='Email'
+          onChange={onChangeEmail}
+          onKeyPress={onKeyPress}
+          value={email}></input>
+        <input
+          className='Pw'
+          type='password'
+          onChange={onChangePw}
+          onKeyPress={onKeyPress}
+          value={pw}></input>
+
+        <img className='Auto' src={ic}></img>
+        <span>자동로그인</span>
+        <div>
+          <button onClick={onLogInClicked}>로그인</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
